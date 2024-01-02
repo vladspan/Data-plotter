e8275186f83fef522782b4c011230dfb9585ab1a
vladspan <117947562+vladspan@users.noreply.github.com>
2024-01-02 15:07:47 +0100

    Update Solution_AVL_Spanic.py
    Latest update.
    User has to first enter the channel and then choose what type of a graph wants to plot out.

--- Solution_AVL_Spanic.py
+++ Solution_AVL_Spanic.py
@@ -1,129 +1,186 @@
+import os
 import numpy as np
-from Index_AVL_Spanic import ChannelIndex
-from Plot_Data_AVL_Spanic import PlotData
+import matplotlib.pyplot as plt
 
+class ChannelIndex:
+    @staticmethod
+    def extract_indices(file_path):
+        try:
+            with open(file_path) as file:
+                lines = [line.rstrip() for line in file]
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {file_path}")
 
-class GraphPlotter():
+        text = ' '.join(lines)
+        text = text.replace(",", " ").replace("&", ",").replace("\n", "").replace("[", "").replace("]", "")
 
+        input_text = text.split("UNIT")[0]
+        channel_ = input_text.split("=")[1]
+        channel_list = channel_.split(",")
 
+        flow_mass_index = None
+        crank_angle_index = None
 
-    global user_input
+        for i, channel in enumerate(channel_list):
+            if channel == " 'Crank Angle' ":
+                crank_angle_index = i
+            elif channel == "  ':flow:total_mass' ":
+                flow_mass_index = i
 
-    user_input = str(input("Hi user :). Please press a, b, c for the type of diagram you want: a) Flow mass @3500 RPM, b) Flow mass @3750 RPM, c) Average Flow mass: "))
+        if crank_angle_index is None or flow_mass_index is None:
+            raise ValueError("Required indices not found in file.")
 
-    def input_and_plot(name: str):
+        return crank_angle_index, flow_mass_index
 
-        if user_input.lower() == "a":
-            with open("003_B1_SN2c2_FL_3500rpm_fl2.txt") as file:
-                lines_a = [line.rstrip() for line in file]
+class PlotData:
+    @staticmethod
+    def plot_data(x, y, rpm, color):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(x, y, linewidth=1, color=color, label=f'Flow Mass @ {rpm} RPM')
+        plt.xticks(np.arange(0, 1281, 85.25))
+        plt.yticks(np.arange(0.1355, 0.1440, 0.0005))
+        plt.title(f'Total Mass @ {rpm} rpm', fontweight="bold")
+        plt.text(0.141, 700, ':flow:total_mass(g)')
+        plt.ylabel('Total Mass (g)')
+        plt.xlabel('Crank Angle (deg)')
+        plt.grid(visible=True, which='major', axis='both', linestyle='dashed')
+        plt.legend(loc='upper left')
+        plt.xlim([0, 1279])
+        plt.ylim([0.1355, 0.143])
+        plt.show()
 
-        elif user_input.lower() == "b":
-            with open("004_B1_SN1c7_FL_3750rpm_fl2.txt") as file:
-                lines_b = [line.rstrip() for line in file]
+    @staticmethod
+    def plot_average(x, y_a, y_b, y_average):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        #ax.plot(x, y_a, linewidth=1, color='red', label='Flow Mass @ 3500 RPM')
+        #ax.plot(x, y_b, linewidth=1, color='green', label='Flow Mass @ 3750 RPM')
+        ax.plot(x, y_average, linewidth=1, color='blue', label='Average Flow Mass')
+        plt.xticks(np.arange(0, 1281, 85.25))
+        plt.yticks(np.arange(0.1355, 0.1440, 0.0005))
+        plt.title(f'Average Total Mass', fontweight="bold")
+        plt.text(0.141, 700, ':flow:total_mass(g)')
+        plt.ylabel('Total Mass (g)')
+        plt.xlabel('Crank Angle (deg)')
+        plt.grid(visible=True, which='major', axis='both', linestyle='dashed')
+        plt.legend(loc='upper left')
+        plt.xlim([0, 1279])
+        plt.ylim([0.1355, 0.143])
+        plt.show()
 
-        elif user_input.lower() == "c":
-            with open("003_B1_SN2c2_FL_3500rpm_fl2.txt") as file:
-                lines_a = [line.rstrip() for line in file]
+class GraphPlotter:
+    def __init__(self):
+        self.user_input = None
+        self.crank_angle = False
 
-            with open("004_B1_SN1c7_FL_3750rpm_fl2.txt") as file:
-                lines_b = [line.rstrip() for line in file]
+    def set_working_directory(self):
+        # Set the working directory to the location of your data files
+        working_directory = "/Users/vladimirspanic/Documents/AVL/AVL_solution"
+        os.chdir(working_directory)
+        print(f"Working directory set to: {os.getcwd()}")
 
-        if user_input.lower() == 'a':
+    def get_user_input(self):
+        self.set_working_directory()
 
-            index = lines_a.index("END")
-            meta_data = lines_a[:index]
-            matrix_data_a = lines_a[index+1:]
+        while True:
+            channel_choose = input("For choosing channel 'Crank angle' press '+': ")
+            if channel_choose == "+":
+                break
+            else:
+                print("Invalid input. Please enter +")
 
-            values_x_a = []
-            values_y_a = []
+        self.crank_angle = True
 
-            for line in matrix_data_a:
-                current_line = line.split("\t")
+        while True:
+            user_input = input("Hi user :). Please press a, b, c for the type of diagram you want: a) Flow mass @3500 RPM, b) Flow mass @3750 RPM, c) Average Flow mass: ")
+            if user_input.lower() in ['a', 'b', 'c']:
+                break
+            else:
+                print("Invalid input. Please enter a, b, or c.")
 
-                values_x_a.append(current_line[main.Index_AVL_Spanic.crank_angle_index])
-                values_y_a.append(current_line[main.Index_AVL_Spanic.flow_mass_index])
+        self.user_input = user_input
 
-            values_y_a_ = list(np.float_(values_y_a))
-            values_y__a = np.array(1000, dtype=np.float64)
-            values_y_gram_a = values_y__a*values_y_a_
+    def input_and_plot(self):
+        self.get_user_input()
 
-            x_plot_a = values_x_a
-            y_plot_a = values_y_gram_a
+        if self.user_input.lower() in ['a', 'b', 'c'] and self.crank_angle:
+            file_paths = {"a": "003_B1_SN2c2_FL_3500rpm_fl2.txt", "b": "004_B1_SN1c7_FL_3750rpm_fl2.txt", "c": None}
+            file_path = file_paths[self.user_input.lower()]
 
-            PlotData.plot_3500(x_plot_a, y_plot_a)
+            if self.user_input.lower() == 'a' or self.user_input.lower() == 'b':
+                crank_angle_index, flow_mass_index = ChannelIndex.extract_indices(file_path)
 
-        if user_input.lower() == 'b':
+                with open(file_path) as file:
+                    lines = [line.rstrip() for line in file]
 
-            index = lines_b.index("END")
-            meta_data = lines_b[:index]
-            matrix_data_b = lines_b[index + 1:]
+                index = lines.index("END")
+                matrix_data = lines[index + 1:]
 
-            values_x_b = []
-            values_y_b = []
+                values_x = []
+                values_y = []
 
-            for line in matrix_data_b:
-                current_line = line.split("\t")
+                for line in matrix_data:
+                    current_line = line.split("\t")
+                    values_x.append(current_line[crank_angle_index])
+                    values_y.append(current_line[flow_mass_index])
 
-                values_x_b.append(current_line[ChannelIndex.crank_angle_index])
-                values_y_b.append(current_line[ChannelIndex.flow_mass_index])
+                values_y_gram = np.array(values_y, dtype=np.float64) * 1000
 
-            values_y_b_ = list(np.float_(values_y_b))
-            values_y__b = np.array(1000, dtype=np.float64)
-            values_y_gram_b = values_y__b * values_y_b_
+                x_plot = values_x
+                y_plot = values_y_gram
 
-            x_plot_b = values_x_b
-            y_plot_b = values_y_gram_b
+                if self.user_input.lower() == 'a':
+                    PlotData.plot_data(x_plot, y_plot, 3500, 'red')
+                elif self.user_input.lower() == 'b':
+                    PlotData.plot_data(x_plot, y_plot, 3750, 'green')
+            else:
+                if self.user_input.lower() == 'c':
+                    file_path_a = "003_B1_SN2c2_FL_3500rpm_fl2.txt"
+                    file_path_b = "004_B1_SN1c7_FL_3750rpm_fl2.txt"
 
-            PlotData.plot_3750(x_plot_b, y_plot_b)
+                    crank_angle_index_a, flow_mass_index_a = ChannelIndex.extract_indices(file_path_a)
+                    crank_angle_index_b, flow_mass_index_b = ChannelIndex.extract_indices(file_path_b)
 
-        if user_input.lower() == 'c':
+                    with open(file_path_a) as file_a:
+                        lines_a = [line.rstrip() for line in file_a]
 
-            index_a = lines_a.index("END")
-            meta_data = lines_a[:index_a]
-            matrix_data_a = lines_a[index_a + 1:]
+                    with open(file_path_b) as file_b:
+                        lines_b = [line.rstrip() for line in file_b]
 
-            index_b = lines_b.index("END")
-            meta_data = lines_b[:index_b]
-            matrix_data_b = lines_b[index_b + 1:]
+                    index_a = lines_a.index("END")
+                    matrix_data_a = lines_a[index_a + 1:]
 
-            values_x_a = []
-            values_y_a = []
+                    index_b = lines_b.index("END")
+                    matrix_data_b = lines_b[index_b + 1:]
 
-            for line in matrix_data_a:
-                current_line = line.split("\t")
+                    values_x_a = []
+                    values_y_a = []
 
-                values_x_a.append(current_line[ch.crank_angle_index])
-                values_y_a.append(current_line[ch.flow_mass_index])
+                    for line in matrix_data_a:
+                        current_line = line.split("\t")
+                        values_x_a.append(current_line[crank_angle_index_a])
+                        values_y_a.append(current_line[flow_mass_index_a])
 
-            values_x_b = []
-            values_y_b = []
+                    values_y_gram_a = np.array(values_y_a, dtype=np.float64) * 1000
 
-            for line in matrix_data_b:
-                current_line = line.split("\t")
+                    values_x_b = []
+                    values_y_b = []
 
-                values_x_b.append(current_line[ch.crank_angle_index])
-                values_y_b.append(current_line[ch.flow_mass_index])
+                    for line in matrix_data_b:
+                        current_line = line.split("\t")
+                        values_x_b.append(current_line[crank_angle_index_b])
+                        values_y_b.append(current_line[flow_mass_index_b])
 
-            values_y_b_ = list(np.float_(values_y_b))
-            values_y__b = np.array(1000, dtype=np.float64)
-            values_y_gram_b = values_y__b * values_y_b_
+                    values_y_gram_b = np.array(values_y_b, dtype=np.float64) * 1000
 
-            values_y_a_ = list(np.float_(values_y_a))
-            values_y__a = np.array(1000, dtype=np.float64)
-            values_y_gram_a = values_y__a * values_y_a_
+                    x_plot_c = values_x_a
+                    y_plot_a = values_y_gram_a
+                    y_plot_b = values_y_gram_b
+                    y_average = np.mean([y_plot_a, y_plot_b], axis=0)
 
-            x_plot_c = values_x_a
+                    PlotData.plot_average(x_plot_c, y_plot_a, y_plot_b, y_average)
+        else:
+            print("Invalid input. Please choose a, b, or c.")
 
-            list_a_b = [values_y_gram_b, values_y_gram_a]
-            values_a_b = [np.array(x) for x in list_a_b]
-            y_plot_c = [np.mean(k) for k in zip(*values_a_b)]
-
-            PlotData.average(x_plot_c, y_plot_c)
-
-    input_and_plot(user_input)
-
-
-
-
-
-
+# Create an instance of GraphPlotter and call input_and_plot method
+graph_plotter = GraphPlotter()
+graph_plotter.input_and_plot()
